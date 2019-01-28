b9b7fb1d9536abf8d98047f083fc5365361e2660
nicotruc <nicolas.frbezar@gmail.com>
2019-01-28 10:49:50 +0100

    Ajout et associations des EIP et des tables de routages

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+# Définition du VPC
 resource "aws_vpc" "main" {
   cidr_block = "${var.VPC_CIDR}"
 
@@ -9,6 +11,8 @@ resource "aws_vpc" "main" {
     Name = "${var.VPC_name}"  
   }
 }
+
+# Creation des VPCs privés et publics
 
 resource "aws_subnet" "vpc_private" {
   count = "${length(var.AZS)}"
@@ -34,7 +38,7 @@ resource "aws_subnet" "vpc_public" {
 }
 
 # Creation de l'internet gateway, porte de sortie de nos réseaux publics
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.main.id}"
 
   tags = {
@@ -48,6 +52,7 @@ resource "aws_key_pair" "mykey" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQve2TG/MUrn2Yvw9Y1YuVabYaF/zR9Fuv6AhhsLsnZSDsxW+nAqeGgQESX3TF7w7PmmQsQdJj/dhDjndjIrAigeOr1CAjFHoYVMjE2k3epO4yivgkpKMNzfJGqJ934jE0X6NHl9767/PeschGFHUlIN2trMcnD6YdL3QWPz2w4yz8dhCp1KPuHbiPZguZSqTZ2i6Rm9cTTdso+APTq4f/tbyHLrA2icswyQ7kFII7VP4RHpOazt7XkBul5eFEA/3/ks8Al7AbpdcRuq7uPIYvD9HmyN2+stzdaBS9xHJs0DIzzXnEhXsZqT3giAabTJhPhLJnf5yc60mlLt7p77+x nicolas.frbezar@gmail.com"
 }
 
+# Récupération du NAT
 data "aws_ami" "nat_ami" {
   most_recent      = true
 
@@ -62,6 +67,7 @@ data "aws_ami" "nat_ami" {
   }
 }
 
+# Creation d'un nat pour chacun des subnets (des AZs)
 resource "aws_instance" "nat" {
   count = "${length(var.AZS)}"
   ami           = "${data.aws_ami.nat_ami.id}"
@@ -73,3 +79,58 @@ resource "aws_instance" "nat" {
   }
 }
 
+# Creation d'EIP pour chacun des NATs
+resource "aws_eip" "nat" {
+  count = "${length(var.AZS)}"
+  vpc      = true
+}
+
+# Association de chaque EIP vers leurs NAT
+resource "aws_eip_association" "eip_assoc_nat" {
+  count = "${length(var.AZS)}"
+  instance_id   = "${element(aws_instance.nat.*.id, count.index)}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+}
+
+# Creation des routings tables pour chacunes des zones de disponibilités privées
+resource "aws_route_table" "private_routing_table" {
+  count = "${length(var.AZS)}"
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${element(aws_instance.nat.*.id, count.index)}"
+  }
+
+  tags = {
+    Name = "${var.VPC_name}-private-${element(var.AZS, count.index)}"
+  }
+}
+
+# Association de chaque subnets à leurs propres NAT
+resource "aws_route_table_association" "private_routing_table_association" {
+  count = "${length(var.AZS)}"
+  subnet_id      = "${element(aws_subnet.vpc_private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private_routing_table.*.id, count.index)}"
+}
+
+# Creation de la routing table des subnets publics
+resource "aws_route_table" "public_routing_table" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags = {
+    Name = "${var.VPC_name}-public"
+  }
+}
+
+# Association des subnets publics vers l'internet gateway
+resource "aws_route_table_association" "public_routing_table_association" {
+  count = "${length(var.AZS)}"
+  subnet_id      = "${element(aws_subnet.vpc_public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public_routing_table.id}"
+}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "private_subnet" {
 output "public_subnet" {
     value = "${aws_subnet.vpc_public.*.id}"
 }
+
+output "EIP" {
+    value = "${aws_eip.nat.*.id}"
+}
